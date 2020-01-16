9809800c27cb6c8fd5a5cacf64054d2ca38c5bfb
onas2729 <karabacakn35@gmail.com>
2020-01-16 18:17:35 -0500

    java file added

---
+++ calculate2.java
@@ -0,0 +1,15 @@
+public class Calculations {
+	public static void main (String [] args){
+		public  int add  (int a, int b){
+
+		}
+
+		public  int divide  (int a, int b){
+			
+		}
+
+		public int subtract   (int a, int b){
+			
+		}
+	}
+}
